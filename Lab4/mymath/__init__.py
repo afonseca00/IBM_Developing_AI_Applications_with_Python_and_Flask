53c2c53560904bec61fb57658fae70c74b9c6b3b
afonseca00 <113251401+afonseca00@users.noreply.github.com>
2024-12-17 15:46:00 +0000

    Create Lab4

---
+++ Lab4/mymath/__init__.py
@@ -0,0 +1,7 @@
+# mymath/__init__.py
+
+from .basic import add, square, double
+from .stats import mean, median
+from .geometry import area_of_rectangle, area_of_circle
+
+__all__ = ['add', 'square', 'double', 'mean', 'median', 'area_of_rectangle', 'area_of_circle']
---
+++ Lab4/mymath/geometry.py
@@ -0,0 +1,13 @@
+import math
+
+def area_of_rectangle(length, width):
+    """
+    This function returns the area of a rectangle given its length and width.
+    """
+    return length * width
+
+def area_of_circle(radius):
+    """
+    This function returns the area of a circle given its radius.
+    """
+    return math.pi * (radius ** 2)
